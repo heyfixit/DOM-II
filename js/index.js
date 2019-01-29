3cf9daf6095c0ce6a52d7d2d415d5dd5ec90c238
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-01-29 16:37:59 -0500

    added body keydown event listener

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 // Your code goes here
 
+function randomRGB() {
+  const vals = [];
+  for(i = 0; i < 3; i++) {
+    vals.push(Math.floor(Math.random() * 255));
+  }
+  return `rgb(${vals.join(",")})`;
+}
+
 window.onload = () => {
   const body = document.getElementsByTagName("body")[0];
   body.style.position = "relative";
@@ -11,8 +19,9 @@ window.onload = () => {
     });
   });
 
-  // body.addEventListener('keydown', (e) => {
-  // });
+  body.addEventListener('keydown', () => {
+    body.style.backgroundColor = randomRGB();
+  });
   // console.log(busImg);
   // busImg.style.position = "absolute"
 
@@ -23,14 +32,17 @@ window.onload = () => {
     // newElement.classList = element.classList;
     newElement.style.position = "absolute";
     newElement.style.zIndex = 999;
-    newElement.style.top = `${rect.top}px`;
-    newElement.style.left = `${rect.left}px`;
+    newElement.style.fontSize = "1.6rem";
+    newElement.style.color = "black";
+    newElement.style.textDecoration = "none";
+    newElement.style.top = `${rect.top + window.scrollY}px`;
+    newElement.style.left = `${rect.left + window.scrollX}px`;
     newElement.style.width = `${rect.right - rect.left}px`;
     newElement.style.transition = "all 0.5s";
     newElement.style.animationTimingFunction = "ease-in";
     setTimeout(() => {
       newElement.style.opacity = 0;
-      newElement.style.transform = `translateY(${(rect.bottom - rect.top + 50)}px`
+      newElement.style.transform = `translateY(${(rect.bottom - rect.top + 250)}px`
     });
     setTimeout(() => newElement.parentNode.removeChild(newElement), 500);
   }
